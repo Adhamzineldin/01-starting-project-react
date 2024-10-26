203b0b40170bfefed7f2cedeb5498eabc6bb1098
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2024-10-26 04:59:48 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     server: {// This is the base path of the app
-        host: '0.0.0.0',  // This makes the app accessible externally
+        // This makes the app accessible externally
         port: 5000,
 
 
